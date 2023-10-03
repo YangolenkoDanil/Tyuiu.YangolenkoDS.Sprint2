c81a9139b8c07586ed5695017d29e13424577b78
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-10-03 17:15:34 +0500

    Add project files.

---
+++ Tyuiu.YangolenkoDS.Sprint2.Task0.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.YangolenkoDS.Sprint2.Task0.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.YangolenkoDS.Sprint2.Task0.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74d75105-2d1b-4f1c-9587-96db62b3974c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.YangolenkoDS.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.YangolenkoDS.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.YangolenkoDS.Sprint2.Task0.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 123;
+            int y = 123;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                         *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                           *");
+            Console.WriteLine("* Задание #0                                                                        *");
+            Console.WriteLine("* Вариант #2                                                                        *");
+            Console.WriteLine("* Выполнил: Янголенко Данил Сергеевич | АСОиУб-23-1                                 *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу из операций сравнений(==, !=, <, >, <=, >=, последовательность *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических выражений,     *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                             *");
+            Console.WriteLine("* (False, True, False, True, False, True), при x = 123, y = 123                     *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
